920432e06e8149dff345c636ce015aab6d91b673
Radim Sückr <radim.suckr@gmail.com>
2023-03-02 15:01:11 +0100

    feat: load config based on env var

--- main.go
+++ main.go
@@ -10,13 +10,19 @@ import (
 )
 
 const (
-	ERROR_LOADING_CONFIG     = 123
-	ERROR_CREATING_WATCHLIST = 124
+	ERROR_LOADING_CONFIG       = 123
+	ERROR_CREATING_WATCHLIST   = 124
+	ERROR_READING_RUMGO_CONFIG = 125
 )
 
 func main() {
+	cfg_path := os.Getenv("RUMGO_CONFIG")
+	if len(cfg_path) < 1 {
+		fmt.Println("set path to config file in \"RUMGO_CONFIG\" env var")
+		os.Exit(ERROR_READING_RUMGO_CONFIG)
+	}
 
-	cfg, cfg_err := config.LoadConfig("./config.json") // TODO: get config path from a env var
+	cfg, cfg_err := config.LoadConfig(cfg_path)
 	if cfg_err != nil {
 		fmt.Println(cfg_err)
 		os.Exit(ERROR_LOADING_CONFIG)
