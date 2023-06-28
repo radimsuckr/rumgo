2c11e5766adb4c10ce310f5daacb7c0477b3f3a5
Radim Sückr <radim.suckr@gmail.com>
2023-06-28 11:52:41 +0200

    refactor: split main into functions

--- main.go
+++ main.go
@@ -15,24 +15,37 @@ const (
 	ErrorReadingRumgoConfig = 125
 )
 
-func main() {
-	cfg_path := os.Getenv("RUMGO_CONFIG")
+func GetConfigPath() (cfg_path string) {
+	cfg_path = os.Getenv("RUMGO_CONFIG")
 	if len(cfg_path) < 1 {
 		fmt.Println("set path to config file in \"RUMGO_CONFIG\" env var")
 		os.Exit(ErrorReadingRumgoConfig)
 	}
+	return cfg_path
+}
 
+func LoadConfig(cfg_path string) (cfg *config.Config) {
 	cfg, err := config.LoadConfig(cfg_path)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(ErrorLoadingConfig)
 	}
+	return cfg
+}
 
+func CreateWatchlist(cfg *config.Config) (watchlist *rules.Watchlist) {
 	watchlist, err := rules.NewWatchlistFromConfig(cfg)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(ErrorCreatingWatchlist)
 	}
+	return watchlist
+}
+
+func main() {
+	cfg_path := GetConfigPath()
+	cfg := LoadConfig(cfg_path)
+	watchlist := CreateWatchlist(cfg)
 
 	for {
 		for _, item := range watchlist.Items {
